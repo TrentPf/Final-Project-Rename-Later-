8546f5f9096648c62928f41ac287520bb698e37b
Anthony <alasam@live.com>
2022-01-16 18:09:32 -0500

    add fix to config/application

--- working-title/config/application.rb
+++ working-title/config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module WorkingTitle
   class Application < Rails::Application
+    config.action_controller.default_protect_from_forgery = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
