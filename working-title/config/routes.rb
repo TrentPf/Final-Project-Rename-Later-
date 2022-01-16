f5170359381bb538cb04bf29e03d35b336a9af99
Trent Pfeiffer <trentpf@hotmail.com>
2022-01-16 17:41:40 -0500

    Add routes, some fixes

--- working-title/config/routes.rb
+++ working-title/config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+
+  resources :tournaments
+  resources :profiles
+
+  # Not certain for this yet
+  resource :make, only: [:show] do
+    post :add_tournament
+    post :remove_tournament
+  end
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
