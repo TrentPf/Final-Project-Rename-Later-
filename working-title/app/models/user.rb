377e8ec5918994ff9edd1bc491296214f385b0a5
Anthony <alasam@live.com>
2022-01-16 19:47:42 -0500

    update session controller and views, add username variable, and update signup and login page

--- working-title/app/models/user.rb
+++ working-title/app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  validates :username,  presence: true, uniqueness: { case_sensitive: false }
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
