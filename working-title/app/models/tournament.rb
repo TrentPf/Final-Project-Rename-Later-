4f40d72294eddcb347fcb475d34f7550f155818e
Trent Pfeiffer <trentpf@hotmail.com>
2022-01-17 17:51:30 -0500

    Implement initial tournament search logic

--- working-title/app/models/tournament.rb
+++ working-title/app/models/tournament.rb
@@ -1,2 +1,18 @@
-class Tournament < ApplicationRecord
+class Tournament < ActiveRecord::Base
+  has_many :entrants
+  has_many :users, through: :entrants
+  validates :name, presence: true
+  validates :game, presence: true
+
+  def self.search(search, game)
+    if search
+      if game
+        self.where("name LIKE ? AND game LIKE ?", "%#{search}%", "%#{game}")
+      else
+        self.where("name LIKE ?", "%#{search}%")
+      end
+    else
+      @tournaments = Tournament.all
+    end
+  end
 end
