f8284a3f4be519a1ea46e6531f6276a5478ff2a8
Riley Pfeiffer <riley_pfeiffer@hotmail.com>
2022-01-18 15:21:58 -0500

    Finish basic landing page and nav fixes

--- working-title/app/models/tournament.rb
+++ working-title/app/models/tournament.rb
@@ -1,2 +1,20 @@
-class Tournament < ApplicationRecord
+class Tournament < ActiveRecord::Base
+  has_many :entrants
+  has_many :users, through: :entrants
+  validates :name, presence: true
+  validates :game, presence: true
+
+  def self.search(search, game)
+    if search
+      caseless = search.downcase
+      caseless = caseless.capitalize
+      if game
+        self.where("name LIKE ? AND game LIKE ?", "%#{caseless}%", "%#{game}%")
+      else
+        self.where("name LIKE ?", "%#{caseless}%")
+      end
+    else
+      @tournaments = Tournament.all
+    end
+  end
 end
--- working-title/db/seeds.rb
+++ working-title/db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+## TOURNAMENTS
+
+tourn1 = Tournament.create({
+  name: "Tournament 1",
+  game: "League of Legends"
+})
+
+tourn2 = Tournament.create({
+  name: "Tournament 2",
+  game: "Super Smash Bros Ultimate"
+})
+
+tourn3 = Tournament.create({
+  name: "Tournament 3",
+  game: "Super Smash Bros Ultimate"
+})
