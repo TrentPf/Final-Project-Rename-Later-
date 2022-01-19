226b85cc566696054334ca655e50c3d4554556c9
Riley Pfeiffer <riley_pfeiffer@hotmail.com>
2022-01-19 18:37:20 -0500

    Add styles to tournament index page

--- working-title/db/schema.rb
+++ working-title/db/schema.rb
@@ -10,15 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_18_194534) do
+ActiveRecord::Schema.define(version: 2022_01_19_031831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "entrants", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
   create_table "tournaments", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
