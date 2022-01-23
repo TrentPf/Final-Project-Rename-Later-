f0941ae91ed356233133b1755fb562aaa4662c80
Anthony <alasam@live.com>
2022-01-23 15:52:13 -0500

    add tournament organized function

--- working-title/app/controllers/application_controller.rb
+++ working-title/app/controllers/application_controller.rb
@@ -7,8 +7,20 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
   helper_method :current_user
 
+  def event_organized
+    @event_organizer = Entrant.where(user_id: current_user.id, organizer: true)
+    @tournaments_organized = []
+    @event_organizer.each do |tournament|
+    @tournaments_organized.push(Tournament.find(tournament.tournament_id))
+    end
+    @tournaments_organized
+  end
+
+  helper_method :event_organized
+  
   def authorize
     redirect_to '/login' unless current_user
   end
--- working-title/app/models/entrant.rb
+++ working-title/app/models/entrant.rb
@@ -1,4 +1,9 @@
 class Entrant < ActiveRecord::Base
   belongs_to :tournament
   belongs_to :user
+
+  def find_entrants
+    @find_entrants = users.where("user_id ILIKE ? AND organizer LIKE ?", "%#{current_user.id}%", true)
+  end
+
 end
